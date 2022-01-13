83f1c121c23a933f7d23e9441b5dcc2a426dd928
TheMasterTz <palavras2001@hotmail.com>
2022-01-13 10:26:59 -0500

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-from fabric.api import run, put, env
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['34.73.110.10', '54.234.193.97']
 
 
 def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        tokenFile = archive_path.split('/')[-1]
-        folderName = tokenFile.split('.')[0]
+        nameFile = archive_path.split("/")[-1]
+        NFile_no_ext = nameFile.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}'.format(path, folderName))
-        run("tar -xzf /tmp/{} -C {}{}/".format(tokenFile, path, folderName))
-        run("rm /tmp/{}".format(tokenFile))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, folderName))
-        run("rm -rf {}{}/web_static".format(path, folderName))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{} /data/web_static/current".format(path, folderName))
+        run('mkdir -p {}{}/'.format(path, NFile_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(nameFile, path, NFile_no_ext))
+        run('rm /tmp/{}'.format(nameFile))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, NFile_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, NFile_no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, NFile_no_ext))
         return True
     except:
         return False
