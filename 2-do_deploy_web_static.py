3bd05f00528237c9b3c9367132c9d04e0fefb29d
TheMasterTz <palavras2001@hotmail.com>
2022-01-13 15:32:44 -0500

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,18 +30,20 @@ def do_pack():
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
 
-    if exists(archive_path):
+    if exists(archive_path) is False:
+        return False
+    try:
         nameFile = archive_path.split("/")[-1]
-        NFile_no_ext = nameFile.split(".")[0]
+        no_ext = nameFile.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, NFile_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(nameFile, path, NFile_no_ext))
-        run('rm /tmp/{}'.format(nameFile))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, NFile_no_ext))
-        run('rm -rf {}{}/web_static'.format(path, NFile_no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, NFile_no_ext))
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(nameFile, path, no_ext))
+        run('sudo rm /tmp/{}'.format(nameFile))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    return False
+    except Exception:
+        return False
---
+++ fabric.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+pip install fabric3
+fab hello
+fab -V
+pip uninstall fabric
+pip uninstall fabric3
+pip uinstall fabric3
+pip install fabric3
+pip uninstall fabric3
+pip install fabric
+fab hello
+pip install Fabric
+sudo apt-get install libffi-dev
+sudo apt-get install libssl-dev
+sudo apt-get install build-essential
+sudo apt-get install python3.4-dev
+sudo apt-get install libpython3-dev
+pip3 install pyparsing
+pip3 install appdirs
+pip3 install setuptools==40.1.0
+pip3 install cryptography==2.8
+pip3 install bcrypt==3.1.7
+pip3 install PyNaCl==1.3.0
+pip3 install Fabric3==1.14.post1
