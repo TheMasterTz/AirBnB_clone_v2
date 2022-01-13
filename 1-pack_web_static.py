026f59db2237ca318ff1188e16a34ff00426a62f
TheMasterTz <palavras2001@hotmail.com>
2022-01-13 10:30:19 -0500

    correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ from os.path import isdir
 
 
 def do_pack():
-    """function"""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
